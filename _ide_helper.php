f20f42e6dc1583f70e76c442beaf486300786dc9
JimChen <18219111672@163.com>
2017-05-14 22:15:51 +0800

    更新composer.lock

--- _ide_helper.php
+++ _ide_helper.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.1.46 (LTS) on 2017-05-11.
+ * Generated for Laravel 5.1.46 (LTS) on 2017-05-14.
  *
  * @author Barry vd. Heuvel <barryvdh@gmail.com>
  * @see https://github.com/barryvdh/laravel-ide-helper
